8cc8f1f77a24e55c92c2fee43679374263603572
cole-marino <cole1.marino@gmail.com>
2023-10-17 08:46:21 -0400

    added pip install

--- src/setup.sh
+++ src/setup.sh
@@ -3,5 +3,8 @@
 #This file installs all dependencies automatically
 # @Author: Cole Marino (cole1.marino@gmail.com)
 
+py get-pip.py
+echo "Installed pip!"
+
 pip install psycopg2-binary
 echo "Installed psycopg2-binary"
