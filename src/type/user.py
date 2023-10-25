42d17c31c227f936f06e36924363aedf2398b653
cole-marino <cole1.marino@gmail.com>
2023-10-25 11:43:55 -0400

    Finished insert with username-checking to make sure there are no duplicates!

--- src/type/user.py
+++ src/type/user.py
@@ -1,3 +1,5 @@
+from datetime import date
+import operations.account as acct
 
 
 
@@ -11,18 +13,33 @@ def insert():
     print("Enter username")
     username = input()
 
-    print("Enter first name")
+    # checks if username exists
+    ret = (acct.check_if_exists(username))
+    if(ret != []):
+        print("\nUsername already exists, please try again.")
+        return insert()
+
+    print("\nEnter password")
+    password = input()
+
+    print("\nEnter first name")
     f_name = input()
 
-    print("Enter last name")
+    print("\nEnter last name")
     l_name = input()
 
-    print("Enter email")
+    print("\nEnter email")
     email = input()
 
-    print("Enter date of birth (usage: [month] [day] [year])")
+    print("\nEnter date of birth (usage: [year]-[month]-[day])\n(usage ex: 2001-09-11 [god bless our troops])")
     dob = input()
+    
+    # gets todays date
+    today = (str)(date.today())
+    today.replace('/', '-')
 
 
     ### Make it check if username already exists
-    return "INSERT INTO Users AS " + username + " (username, )"
+    cmd = "INSERT INTO users(username, name, email, password, dob, create_date) VALUES ('"+username+"', '"+(f_name+" "+l_name)+"', '"+email+"', '"+password+"', '"+dob+"', '"+today+"');"
+    print(cmd)
+    return cmd
