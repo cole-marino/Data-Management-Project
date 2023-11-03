906ad10f904456ac965d4bafce53e2534da9016f
cole-marino <cole1.marino@gmail.com>
2023-11-03 14:00:22 -0400

    Removed book list stuff from here..

--- src/operations/book.py
+++ src/operations/book.py
@@ -9,72 +9,6 @@ import command_prompt as cp
 
 from datetime import datetime
 
-def view_user_list(username, list_name):
-    '''
-    Prints all the books that are in a users list.
-    @param username: The users username
-    @param list_name: The name of the list being printed.
-    @return none
-    '''
-    command = "SELECT b.title AS bookname,\
-       CONCAT(p.fname, ' ', p.lname) AS author,\
-       br.startdate,\
-       br.enddate,\
-       br.pagesread\
-    FROM bookreads br\
-    JOIN book b ON br.bid = b.bid\
-    JOIN authors a ON b.bid = a.bid\
-    JOIN person p ON a.cid = p.cid\
-    JOIN bookslist bl ON br.bid = bl.bid\
-    JOIN users u ON bl.username = u.username\
-    WHERE u.username = '"+username+"'\
-    AND bl.listname = '"+list_name+"';"
-    
-    result = cp.execute_sql(command)
-
-    # Prints books that are in this list
-    for i in range(len(result)):
-        print("Book " + str(i+1) + ") " + result[i][0]) # Prints book title
-        print("\tAuthor(s): " + result[i][1])
-        print("\tRelease date: "+ result[i][2].strftime("%m/%d/%y"))
-        print("\tPages: " + str(result[i][4]))
-
-
-
-def get_user_lists(username):
-    '''
-    Gets all the lists which a user has.
-    @param username: Users username.
-    @return: None
-    '''
-    command = "SELECT bl.username AS listowner,\
-            bl.listname,\
-            COUNT(bl.bid) AS numberofbooks, \
-            SUM(b.length) AS totallength \
-            FROM bookslist bl  \
-            JOIN book b ON bl.bid = b.bid \
-            WHERE bl.username = '"+username+"' \
-            GROUP BY bl.username, bl.listname  \
-            ORDER BY listname ASC, listowner;" 
-            
-    
-    #options to view lists otherwise       
-    result = cp.execute_sql(command)
-    
-    print("\n") # spacing
-    for i in range(0, len(result)):
-        print(str(i+1) +") '"+ result[0][1] + "' || Number of Books: " + str(result[0][2]) + " || Total Pages: " + str(result[0][2]))
-        
-    list_num = int(input("\nEnter list number to view list, hit enter to return to menu.\n"))
-    print() # creates spacing
-    if isinstance(list_num, int):
-        view_user_list(username, result[list_num][1])
-    
-    # creates spacing
-    print()
-
-    return None
-
 
 def book_Search_parse():
     """
@@ -163,7 +97,7 @@ def book_Search_cmd(name, r_date, author, publisher, genre):
     return cmd_book
 
 
-def book_Rate_parse(username):
+def book_rate_parse(username):
     """
     "Houses the bookRate Function for use in the command prompt interface. provides a user prompt for info."
     :return: SQL command
@@ -172,11 +106,11 @@ def book_Rate_parse(username):
     prompt = input("Please provide the book and its rating (1-5)\n"
                    "Usage: [book], [rating]\n")
     cmd_input = prompt.strip().split(", ")
-    out = book_Rate(cmd_input[0], cmd_input[1], username)
+    out = book_rate(cmd_input[0], cmd_input[1], username)
     return out
 
 
-def book_Rate(book, rating, username):
+def book_rate(book, rating, username):
     """
     returns an SQL command as a string to rate the book as named by the variable book
     :param book: exact title of the book
